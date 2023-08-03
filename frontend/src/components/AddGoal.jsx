f48c9b19ee347ea1d9c433023266dd6421c1f515
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-08-03 10:28:48 -0700

    update addGoal

--- frontend/src/components/AddGoal.jsx
+++ frontend/src/components/AddGoal.jsx
@@ -70,11 +70,11 @@ const AddGoal = () => {
 
             <form onSubmit={submitHandler}>
                 <div className='my-4 title'>
-                    <label className='mb-2'>Goal Title</label>
+                    <label className='mb-2'>Enter Goal Title</label>
                     <input 
                         className='block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                         type='text'
-                        placeholder='Enter goal title. Be as specific as you like.'
+                        placeholder='Be as specific as you like.'
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         aria-describedby="titleHelpBlock"
@@ -85,7 +85,7 @@ const AddGoal = () => {
                 </div>
                 <hr />
                 <div className='my-4 endDate'>
-                    <label className='mb-2'>End Date</label>
+                    <label className='mb-2'>Select an End Date</label>
                     <input
                         className='block w-full rounded-md border-0 pt-0.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 align-middle focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                         type="date"
@@ -106,7 +106,7 @@ const AddGoal = () => {
                 <div className="mt-5 flex lg:ml-4 lg:mt-0">
                     <span className=" mr-2 hidden sm:block">
                     <button
-                        type="button"
+                        type="submit"
                         className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         disabled={isLoading}
                     >
@@ -117,7 +117,7 @@ const AddGoal = () => {
                     <button
                         type="button"
                         className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        onClick={() => setShowAddGoal(false)}
+                        onClick={handleAddClick}
                     >
                     Cancel
                     </button>
