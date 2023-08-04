6db3c9df221be8e3f8c637cfc236b1a93c7dfe69
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-08-03 17:53:54 -0700

    add goal

--- frontend/src/components/AddGoal.jsx
+++ frontend/src/components/AddGoal.jsx
@@ -10,6 +10,15 @@ const AddGoal = () => {
     const [isPublic, setIsPublic] = useState(true);
     const [showAddGoal, setShowAddGoal] = useState(false)
 
+    const handleAddClick = () => {
+        if(showAddGoal) {
+            document.getElementById('addGoalOutline').classList.remove('border-2');
+        } else {
+            document.getElementById('addGoalOutline').classList.add('border-2');
+        }
+        setShowAddGoal(!showAddGoal)
+    }
+
     
     const navigate = useNavigate();
 
@@ -45,69 +54,80 @@ const AddGoal = () => {
       
     return (
         <>
-        <button
-            type='button'
-            className='my-3'
-            onClick={() => setShowAddGoal(true)}
-        >
+        <section className='flex-initial max-w-sm min-h-full flex-col justify-left px-6 py-2 lg:px-8'>
+        <span>
+            <button
+                type="button"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleAddClick}
+            >
             Add Goal
-        </button>
-        <div> { showAddGoal ? 
-        <FormContainer>
-            <h1>Add Goal</h1>
+            </button>
+        </span>
+        <div id='addGoalOutline' className='rounded-md border-indigo-600 mt-4'> { showAddGoal ? 
+        <FormContainer className="flex min-h-full flex-col justify-center px-6 py-6">
+            <h1 className='text-xl font-bold text-gray-600'>Add Goal</h1>
 
             <form onSubmit={submitHandler}>
-                <div className='my-2 title'>
-                    <label>Goal Title</label>
+                <div className='my-4 title'>
+                    <label className='mb-2'>Goal Title</label>
                     <input 
+                        className='block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                         type='text'
-                        placeholder='Enter goal title'
+                        placeholder='Enter goal title. Be as specific as you like.'
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         aria-describedby="titleHelpBlock"
                     ></input>
-                    <span>
-                        Enter your goal title here. Be as specific as you like. For example: Play guitar for 30 minutes every day for 1 year or Cook at home today. 
+                    <span className='text-xs text-gray-400'>
+                        Eg: Play guitar for 30 minutes every day for 1 year or Cook at home today. 
                     </span>
                 </div>
-                <div className='my-2 endDate'>
-                    <label>End Date</label>
+                <hr />
+                <div className='my-4 endDate'>
+                    <label className='mb-2'>End Date</label>
                     <input
+                        className='block w-full rounded-md border-0 pt-0.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 align-middle focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                         type="date"
-                        name="datepic"
-                        placeholder="End Date"
                         value={endDate}
                         onChange={(e) => setEndDate(e.target.value)}
                     />
                 </div>
-                <div className='my-2 isPublic'>
-                    <label>Make your goal private?</label>
+                <hr />
+                <div className='my-4 isPublic'>
+                    <label className='mb-2 mr-4'>Make your goal private?</label>
                     <input 
-                        type="switch"
-                        label="Make your goal private?"
+                        type="checkbox"
+                        defaultChecked={isPublic}
                         aria-label='Make your goal private?'
                         onChange={togglePublic}
                     ></input>
                 </div>
-                <button
-                    type= 'submit'
-                    className='mt-3'
-                    disabled={isLoading}
-                >
+                <div className="mt-5 flex lg:ml-4 lg:mt-0">
+                    <span className=" mr-2 hidden sm:block">
+                    <button
+                        type="button"
+                        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        disabled={isLoading}
+                    >
                     {isLoading ? 'Loading…' : 'Submit'}
-                </button>
-                {'   '}
-                <button 
-                    type='button' 
-                    className='mt-3'
-                    onClick={() => setShowAddGoal(false)}
-                >
+                    </button>
+                    </span>
+                <span className="ml-2 hidden sm:block">
+                    <button
+                        type="button"
+                        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        onClick={() => setShowAddGoal(false)}
+                    >
                     Cancel
-                </button>
+                    </button>
+                </span>
+                </div>
             </form>
 
         </FormContainer>
         : null } </div>
+        </section>
         </>
     )};
 
