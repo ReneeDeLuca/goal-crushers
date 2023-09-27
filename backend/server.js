2186d1d8838ea7b0d95ca3669edabc89bc7d1448
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-26 18:04:39 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
-import cookieParser from 'cookie-parser';
-import {notFound, errorHandler} from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
-const port = process.env.PORT || 3001;
-import userRoutes from './routes/userRoutes.js';
-import goalRoutes from './routes/goalRoutes.js';
-import mainRoutes from './routes/mainRoutes.js';
-import profileRoutes from './routes/profileRoutes.js';
-import commentRoutes from './routes/commentRoutes.js';
-import statusRoutes from './routes/statusRoutes.js';
-import imageRoutes from './routes/imageRoutes.js';
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const port = process.env.PORT || 3000;
+import userRoutes from "./routes/userRoutes.js";
+import goalRoutes from "./routes/goalRoutes.js";
+import mainRoutes from "./routes/mainRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import statusRoutes from "./routes/statusRoutes.js";
+import imageRoutes from "./routes/imageRoutes.js";
 
 const app = express();
 
@@ -21,29 +21,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //Routes
-app.use('/api/main', mainRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/goal', goalRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/comment', commentRoutes);
-app.use('/api/status', statusRoutes);
-app.use('/api/images', imageRoutes);
+app.use("/api/main", mainRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/goal", goalRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/comment", commentRoutes);
+app.use("/api/status", statusRoutes);
+app.use("/api/images", imageRoutes);
 
-
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
-
 
 //Error Handler Middleware - otherwise express will return HTML error page
 app.use(notFound);
@@ -52,6 +49,6 @@ app.use(errorHandler);
 //Connect to the database before listening
 connectDB().then(() => {
   app.listen(port, () => {
-      console.log("listening for requests");
-  })
-})
+    console.log("listening for requests");
+  });
+});
