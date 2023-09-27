7a47e67562e0a9a9aed1051282322f0de09552d3
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-26 17:53:27 -0700

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {fileURLToPath} from 'url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 const defaultConfig = {
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-}
+};
 export default defineConfig(({ command, mode }) => {
-  if (command === 'serve') {
-    const isDev = mode === 'development'
+  if (command === "serve") {
+    const isDev = mode === "development";
     return {
       ...defaultConfig,
-    server:{
-      proxy: {
-        '/api': {
-          target: isDev ? 'http://localhost:3000' : 'https://goalcrushers.cyclic.cloud/',
-          changeOrigin: isDev,
-          secure: !isDev,
-        }
-      }
-    }
-    } 
+      server: {
+        proxy: {
+          "/api": {
+            target: isDev
+              ? "http://localhost:3000"
+              : "https://goalcrushers.cyclic.cloud/",
+            changeOrigin: isDev,
+            secure: !isDev,
+          },
+        },
+      },
+    };
   } else {
-    return defaultConfig
+    return defaultConfig;
   }
-})
-      
-
-
+});
