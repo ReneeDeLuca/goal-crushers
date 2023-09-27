f373f07bced4bf38afc2a9bf0ef286fa819c08bd
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-26 19:35:13 -0700

    update package.json, vite.config.js, frontend/package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-const defaultConfig = {
+export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  server: {
+    host: "0.0.0.0",
   },
-};
-export default defineConfig(({ command, mode }) => {
-  if (command === "serve") {
-    const isDev = mode === "development";
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          "/api": {
-            target: isDev
-              ? "http://localhost:3000"
-              : "https://goalcrushers.cyclic.cloud/",
-            changeOrigin: isDev,
-            secure: !isDev,
-          },
-        },
-      },
-    };
-  } else {
-    return defaultConfig;
-  }
 });
