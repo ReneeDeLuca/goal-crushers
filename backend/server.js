98716bc93e54d50841698a3571f822631e90ed0f
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-28 09:07:20 -0700

    update gitignore

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"), (err) => {
-      res.status(500).send(err);
-    });
+    res.sendFile(
+      path.join(__dirname, "frontend", "dist", "index.html"),
+      (err) => {
+        res.status(500).send(err);
+      }
+    );
   });
 } else {
   app.get("/", (req, res) => {
