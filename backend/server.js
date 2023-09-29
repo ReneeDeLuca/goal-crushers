eac92bf14e51d1ad6df9c140873d5b1e2cadfb0b
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-29 08:58:43 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "app", "frontend", "dist", "index.html"),
+      path.join(__dirname, "frontend", "dist", "index.html"),
       (err) => {
         res.status(500).send(err);
       }
