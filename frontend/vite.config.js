1dda6ecab1d8a36b23ef328df1f2785435d05607
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-09-28 17:57:37 -0700

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,6 +25,12 @@ export default defineConfig({
       },
     },
   ],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[local]_[hash:base64:2]",
+    },
+  },
   server: {
     host: "0.0.0.0",
   },
