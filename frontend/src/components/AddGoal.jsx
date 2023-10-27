43c6fde9ae913ff53074e888243a3bd45aa3c700
Renee DeLuca <111710583+ReneeDeLuca@users.noreply.github.com>
2023-10-26 20:07:07 -0700

    update AddGoal.jsx

--- frontend/src/components/AddGoal.jsx
+++ frontend/src/components/AddGoal.jsx
@@ -164,7 +164,7 @@ const AddGoal = () => {
                         type="button"
                         id="submit"
                         className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        disabled={!canSave}
+                        disabled={isLoading || !canSave}
                         onClick={submitHandler}
                       >
                         {isLoading ? "Loading…" : "Submit"}
